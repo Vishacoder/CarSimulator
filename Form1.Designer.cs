032991387a105f9159d038eafa5d5db55f1ffc74
Rankoth Vishwa Anupama <vishwaanupama12@gmail.com>
2025-07-15 21:13:44 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace CarSimulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.carBox = new System.Windows.Forms.PictureBox();
+            this.mainTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.carBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // carBox
+            // 
+            this.carBox.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.carBox.Location = new System.Drawing.Point(100, 100);
+            this.carBox.Name = "carBox";
+            this.carBox.Size = new System.Drawing.Size(50, 30);
+            this.carBox.TabIndex = 0;
+            this.carBox.TabStop = false;
+            // 
+            // mainTimer
+            // 
+            this.mainTimer.Enabled = true;
+            this.mainTimer.Interval = 50;
+            this.mainTimer.Tick += new System.EventHandler(this.mainTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.carBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.carBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox carBox;
+        private System.Windows.Forms.Timer mainTimer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CarSimulator
+{
+    public partial class Form1 : Form
+    {
+        int carSpeed = 5;
+        bool goLeft, goRight, goUp, goDown;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Nothing here for now
+        }
+
+        private void mainTimer_Tick(object sender, EventArgs e)
+        {
+            if (goLeft && carBox.Left > 0) carBox.Left -= carSpeed;
+            if (goRight && carBox.Right < this.ClientSize.Width) carBox.Left += carSpeed;
+            if (goUp && carBox.Top > 0) carBox.Top -= carSpeed;
+            if (goDown && carBox.Bottom < this.ClientSize.Height) carBox.Top += carSpeed;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left) goLeft = true;
+            if (e.KeyCode == Keys.Right) goRight = true;
+            if (e.KeyCode == Keys.Up) goUp = true;
+            if (e.KeyCode == Keys.Down) goDown = true;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left) goLeft = false;
+            if (e.KeyCode == Keys.Right) goRight = false;
+            if (e.KeyCode == Keys.Up) goUp = false;
+            if (e.KeyCode == Keys.Down) goDown = false;
+        }
+    }
+}
